5d83b32bae38712398e7f8db62ed4c74995dad10
somminn <cowmin104@gmail.com>
2025-09-04 18:01:38 +0900

    [Fix] 상품 검색 시 보유하고있는 농장만 출력

--- backend/src/main/java/org/example/groworders/domain/farms/service/FarmService.java
+++ backend/src/main/java/org/example/groworders/domain/farms/service/FarmService.java
@@ -1,6 +1,5 @@
 package org.example.groworders.domain.farms.service;
 
-import io.micrometer.common.lang.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.groworders.config.push.event.PushEvent;
@@ -11,7 +10,6 @@ import org.example.groworders.domain.farms.repository.FarmRepository;
 import org.example.groworders.domain.users.service.S3PresignedUrlService;
 import org.springframework.context.ApplicationEventPublisher;
 import org.example.groworders.domain.users.service.S3UploadService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,47 +22,44 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FarmService {
     private final FarmRepository farmRepository;
-    private final CropRepository cropRepository;
     private final S3UploadService s3UploadService; //업로드
     private final S3PresignedUrlService s3PresignedUrlService; //불러오기
     private final ApplicationEventPublisher publisher;
 
-    @Value("${spring.cloud.aws.s3.bucket}")
-    private String s3BucketName;
-
     // 농장 등록
     @Transactional
     public FarmDto.FarmResponse register(FarmDto.Register dto,
-                                         @Nullable MultipartFile farmImageUrl,
+                                         MultipartFile farmImageUrl,
                                          Long userId) throws IOException {
         // 농장 이미지 업로드
         String filePath = s3UploadService.upload(farmImageUrl);
         Farm farm = farmRepository.save(dto.toEntity(userId, filePath));
 
-        // 테스트용: 농장 등록 시 농부에게 알림 전송
+        // 푸시 테스트용: 농장 등록 시 농부에게 알림 전송
         farmRegisterPush(farm);
-
         return FarmDto.FarmResponse.from(farm);
     }
 
     // 농장 정보
     @Transactional(readOnly = true)
-    public FarmDto.FarmResponse read(Long id) {
+    public FarmDto.FarmListResponse read(Long id) {
         Farm farm = farmRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("농장을 찾을 수 없습니다."));
-        return FarmDto.FarmResponse.from(farm);
+            .orElseThrow(() -> new IllegalArgumentException("농장을 찾을 수 없습니다."));
+        String presignedUrl = farm.getFarmImage() != null ?
+                s3PresignedUrlService.generatePresignedUrl(farm.getFarmImage(), Duration.ofMinutes(60)) :
+                s3PresignedUrlService.generatePresignedUrl("not-found-image.jpg", Duration.ofMinutes(60));
+        return FarmDto.FarmListResponse.from(farm, presignedUrl);
     }
 
     // 농장 정보 수정
     @Transactional
     public FarmDto.FarmResponse update(Long farmId,
                                        FarmDto.Update dto,
-                                       @Nullable MultipartFile image,
                                        Long id) {
         Farm farm = farmRepository.findById(farmId)
                 .orElseThrow(() -> new IllegalArgumentException("농장을 찾을 수 없습니다."));
 
-        // 소유자 검증 (권장)
+        // 소유자 검증
         if (!farm.getUser().getId().equals(id)) {
             throw new SecurityException("수정 권한이 없습니다.");
         }
@@ -74,7 +69,7 @@ public class FarmService {
 
     // 농장 리스트
     public List<FarmDto.FarmListResponse> listAll() {
-        List<Farm> farmList = farmRepository.findAll();
+        List<Farm> farmList = farmRepository.findFarmsHavingAnyCrop();
         return farmList.stream().map(farm -> {
             String presignedUrl = farm.getFarmImage() != null ?
                     s3PresignedUrlService.generatePresignedUrl(farm.getFarmImage(), Duration.ofMinutes(60)) :
@@ -83,7 +78,7 @@ public class FarmService {
         }).toList();
     }
 
-    // 농장 등록 알림 발송
+    // 푸시 테스트용: 농장 등록 알림 발송
     public void farmRegisterPush(Farm farm) {
         publisher.publishEvent(PushEvent.builder()
                 .userId(farm.getUser().getId())
