699192bd38dd73fe8f3c0fcbcb4b034ea57da0cf
somminn <cowmin104@gmail.com>
2025-09-04 21:16:18 +0900

    [Update] commit

--- backend/src/main/java/org/example/groworders/config/notification/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.groworders.config.notification.repository;
-
-import org.example.groworders.config.notification.model.entity.NotificationSub;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<NotificationSub, Long>{
-}
